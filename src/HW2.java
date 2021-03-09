6c9197c69e6c076165267dd7a1f9d480bcf354d5
Tetyana-hr <grivastg@gmail.com>
2021-03-10 00:17:29 +0200

    add HW2

--- src/HW2.java
+++ src/HW2.java
@@ -1,18 +1,24 @@
 public class HW2 {
     public static void main(String[] args) {
         int a = 19;
-        int b = 5;
+        int b = 6;
 
-        int rest = a % b;
         System.out.println("Addition : " + (a + b));
         System.out.println("Subtraction : " + (a - b));
         System.out.println("Multiplication : " + (a * b));
         System.out.println("Division : " + ((0.0 + a) / b));
-        System.out.println("Rest : " + rest);
-        if (rest == 0){
-            System.out.println("The number is even !");
+        System.out.println("Rest : " + (a % b));
+
+        if ((a % 2) == 0){
+            System.out.println("The number a is even !");
         } else {
-            System.out.println("The number is not even !");
+            System.out.println("The number a is not even !");
+        }
+
+        if ((b % 2) == 0){
+            System.out.println("The number b is even !");
+        } else {
+            System.out.println("The number b is not even !");
         }
 
         System.out.println("\nExtra task\n");
